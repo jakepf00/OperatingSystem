51859f602c058441ec42da3199dec76240d25e94
Jacob Pfeifer <jakepf00@gmail.com>
2025-04-21 08:29:01 -0500

    Add basic processes and context switching

--- kernel.h
+++ kernel.h
@@ -62,3 +62,13 @@ struct sbiret {
     long error;
     long value;
 };
+
+#define PROCS_MAX 8 // Maximum number of processes
+#define PROC_UNUSED 0 // Unused process control structure
+#define PROC_RUNNABLE 1 // Runnable process
+struct process {
+    int pid; // Process ID
+    int state; // Process state: PROC_UNUSED or PROC_RUNNABLE
+    vaddr_t sp; // Stack pointer
+    uint8_t stack[8192]; // Kernel stack
+};
